b008e58378398c2745affa1dd8158d95a53e3bcd
Nathan Birrell <nathanbirrell@gmail.com>
2016-04-19 23:43:06 +1000

    Structured WP enqueues for CSS properly

--- wp-content/themes/sturlly-child-theme/functions.php
+++ wp-content/themes/sturlly-child-theme/functions.php
@@ -9,10 +9,11 @@
 function sturlly_parent_styles() {
 	$parent = get_template();
 	$parent = wp_get_theme($parent);
-	
+
 	wp_enqueue_style( 'sturlly-child-theme', get_template_directory_uri() . '/style.css', array(), $parent['Version'], 'all');
 	
-	wp_enqueue_style('allcity_style', get_stylesheet_directory_uri() . '/assets/css/style.css');
-	wp_enqueue_script('allcity_script', get_stylesheet_directory_uri() . '/assets/js/script.js');
+	wp_enqueue_style('allcity_style', get_stylesheet_directory_uri() . '/assets/css/style.css', array('custom-style','portfolio-style','fancybox','scrollbar','responsive','prettyPhoto'));
+
+	wp_enqueue_script('allcity_script', get_stylesheet_directory_uri() . '/assets/js/script.js', array( 'jquery' ), false, true);
 }
 add_action('wp_enqueue_scripts', 'sturlly_parent_styles');
