9fa9814f69dce5240c8eadfc9126a5ed110cbec3
Nathan Birrell <nathanbirrell@gmail.com>
2016-04-27 09:54:31 +1000

    Remove padding from top of Youtube video

--- wp-content/themes/sturlly-child-allcity/assets/js/script.js
+++ wp-content/themes/sturlly-child-allcity/assets/js/script.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function() {
     // remove weird white text that gets set mysteriously
 	$('.work-count p.content-white').removeClass('content-white');
 
+	$('.fluid-width-video-wrapper').css('padding-top', '0');
+
 	var clipboard = new Clipboard('.copy-to-clipboard');
 
 	clipboard.on('success', function(e) {
