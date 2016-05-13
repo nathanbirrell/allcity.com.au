7ce949f84b4115ffaa4ff64b74a0bc71f063d3c5
Nathan Birrell <nathanbirrell@gmail.com>
2016-05-13 17:02:05 +1000

    Make CTA in footer an include

--- wp-content/themes/sturlly-child-allcity/footer.php
+++ wp-content/themes/sturlly-child-allcity/footer.php
@@ -18,52 +18,8 @@ $fsocial_rss        = cs_get_option('fsocial_rss');
 $csocial_intagram   = cs_get_option('csocial_intagram');
 ?>
 
-<section id="contact" class="page-section">
-   <div class="content-section pt-0-pb-65">
-      <div class="container">
-         <div class="row contact-us">
-            <div class="col-md-12">
-               <div class="title-style2 title-text">
-                  <h2 class="title text-center">Book your free consultation today</h2>
-                  <div class="green-line"></div>
-               </div>
-               <div class="text-center title-style2 clear-both">
-                  <div class="col-xs-10 title-style2 col-md-6 col-sm-8 text-center description margin-top margin-bottom margin-auto-center gray-text">Arrange an appointment at the showroom to make your dream home a reality.</div>
-               </div>
-               <div class="content-section no-padding">
-                  <div class="container">
-                     <div class="row">
-                        <div class="col-md-4 col-sm-6 no-padding"></div>
-                        <div class="col-md-4">
-                           <div class="contact-style1">
-                              <div class="success callout hidden" data-closable>
-                                <p>Copied to clipboard</p>
-                                <!-- <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-                                  <span aria-hidden="true">&times;</span>
-                                </button> -->
-                              </div>
-                              <ul class="icon-list">
-                                <li class="lead mailchimp-popup" id="cta-arrange-consultation"><button>Arrange a Consultation</button></li>
-                                <li class="divider"></li>
-                                 <li class="phone">
-                                    <button id ="cta-call-main" data-clipboard-text="03 9571 7000" class="copy-to-clipboard" title="Click to copy"><i class="fa fa-phone"></i>03 9571 7000</button>
-                                    <a id ="cta-call-main-mob" href="tel:0395717000" class="cta-contact-call" title="Call the All City showroom"><i class="fa fa-phone"></i>03 9571 7000</a>
-                                 </li>
-                                 <li class="divider"></li>
-                                 <li><a href="mailto:info@allcity.com.au"><i class="fa fa-envelope-o"></i>info@allcity.com.au</a></li>
-                                 <li class="divider"></li>
-                                 <li><a href="https://www.google.com.au/maps/place/All+City+Bathrooms+%26+Kitchens/@-37.8796416,145.0695946,15z" target="_blank"><i class="fa fa-map-marker"></i>Visit the Showroom</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-</section>
+
+<?php include("includes/cta-footer.php"); ?>
 
 <!-- footer -->
 <footer id="footer" class="footer footer-style1" style="background-image:url('<?php echo esc_url($footer_bg); ?>');">
