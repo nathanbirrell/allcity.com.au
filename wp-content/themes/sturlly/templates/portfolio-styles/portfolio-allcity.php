24ebe29a4be657c12dd0000d3dcc3b648594eea9
Nathan Birrell <nathanbirrell@gmail.com>
2016-05-18 21:34:57 +1000

    Fix gallery image order bug

--- wp-content/themes/sturlly/templates/portfolio-styles/portfolio-allcity.php
+++ wp-content/themes/sturlly/templates/portfolio-styles/portfolio-allcity.php
@@ -12,7 +12,7 @@ $post_category = get_the_terms($post->ID, 'portfolio-category');
 $slug = '';
 $category = '';
 
-foreach($post_category as $cat) { 
+foreach($post_category as $cat) {
   $slug .= $cat->slug.' ';
   $category .= $cat->slug.', ';
 }
@@ -20,15 +20,15 @@ foreach($post_category as $cat) {
 $post_options    = get_post_meta( $post_id, '_custom_portfolio_options', true );
 $gallery_images  = (!empty($post_options['pt_gallery'])) ? $post_options['pt_gallery']:'';
 $image_array     = explode(',', $gallery_images);
-?> 
+?>
 
 
 <li class="portfolio-item item col-md-3 col-sm-3 gallery <?php echo sanitize_html_classes($slug); ?> " data-rel="popup-<?php print $post->ID;?>">
-  <figure class="portfolio-figure gallery-thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"> 
-    <?php if(has_post_thumbnail()) { 
+  <figure class="portfolio-figure gallery-thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">
+    <?php if(has_post_thumbnail()) {
       $thumb_id = get_post_thumbnail_id( $post_id );
       $image_src_obj = wp_get_attachment_image_src( $thumb_id, 'large' );
-      $image_src = esc_url($image_src_obj[0]); 
+      $image_src = esc_url($image_src_obj[0]);
       $image_width = $image_src_obj[1];
       $image_height = $image_src_obj[2];
       $image_title = get_the_title($thumb_id);
@@ -41,12 +41,12 @@ $image_array     = explode(',', $gallery_images);
   <?php if( ! empty( $gallery_images ) ) { ?>
     <?php foreach( $image_array as $key => $image_id ) {
         $image_src_obj  = wp_get_attachment_image_src( $image_id, 'large' );
-        $image_src = esc_url($image_src_obj[0]); 
+        $image_src = esc_url($image_src_obj[0]);
         $image_width = $image_src_obj[1];
         $image_height = $image_src_obj[2];
         $image_title = get_the_title($image_id);
 
-        if ($image_id == get_post_thumbnail_id( $post_id )) {return;} // check for duplicate
+        if ($image_id == get_post_thumbnail_id( $post_id )) {continue;} // check for duplicate
       ?>
       <figure class="hidden" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">
         <a href="<?= $image_src ?>" title="<?= $image_title ?>" data-size="<?= $image_width ?>x<?= $image_height ?>">
