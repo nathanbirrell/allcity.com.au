b0e27f54b5ed11b9b910b8fc78f111e4f2d7b8bb
Nathan Birrell <nathanbirrell@gmail.com>
2016-12-27 17:36:50 +1100

    Make styles compatible with a WPForms consultation form

--- wp-content/themes/sturlly-child-allcity/assets/js/script.js
+++ wp-content/themes/sturlly-child-allcity/assets/js/script.js
@@ -10,6 +10,14 @@ ALLCITY.init = function() {
   // hide a bunch of theme crap
   $('.comments-main, .month, .medium.light-gray').hide();
 
+  // add material design bar to consultation form
+  $('.wpforms-field').append('<i class="bar"></i>');
+  // move material design label below input element 
+  $('.wpforms-field').each(function(i, field) {
+  	var label = $(field).children('.wpforms-field-label');
+  	label.insertBefore($(field).children('.bar'));
+  });
+
   var clipboard = new Clipboard('.copy-to-clipboard');
 
   clipboard.on('success', function(e) {
@@ -26,7 +34,7 @@ ALLCITY.init = function() {
   });
 
   $('#contact .content-section').on('click', function(event) {
-    console.log('close');
+    console.log('stopped click event propagation');
     event.stopPropagation();
   });
 
